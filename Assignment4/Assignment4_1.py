15ad48d2bd249e1996bc6071d3d9c4ef49371966
Apurva Jogal <43969525+ApurvaJogal@users.noreply.github.com>
2019-03-21 00:15:59 +0530

    Add files via upload

---
+++ Assignment4/Assignment4_1.py
@@ -0,0 +1,13 @@
+# Write a program which contains one lambda function which accepts one parameter and return power of two.
+# Input : 4 Output : 16
+# Input : 6 Output : 64
+
+#	Author : Apurva Anil Jogal
+#	Date   : 20th March 2019
+
+
+
+result = lambda x: 2 ** x;
+no=input("Enter number:");
+
+print( result(no));
---
+++ Assignment4/Assignment4_2.py
@@ -0,0 +1,15 @@
+#Write a program which contains one lambda function which accepts two parameters and return its multiplication.
+#Input : 4	3 	Output : 12
+#Input : 6 	3 	Output : 18
+
+
+
+#	Author : Apurva Anil Jogal
+#	Date   : 20th March 2019
+
+
+result = lambda a,b: a*b;
+no1=input("Enter first number:");
+no2=input("Enter second number:");
+
+print( result(no1,no2));
